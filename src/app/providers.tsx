d16522058fbe1981559d4d257faa693b4c9d2a6f
Romaric Zounlome <rzounlom@gmail.com>
2024-01-13 12:23:24 -0500

    chore: install/configure clerk

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,16 @@
 "use client";
 
+import { ClerkProvider } from "@clerk/nextjs";
 import { Provider } from "react-redux";
 import { ThemeProvider } from "next-themes";
-import { store } from "./state/store";
+import { store } from "@/state/store";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <Provider store={store}>
-      <ThemeProvider attribute="class">{children}</ThemeProvider>
+      <ClerkProvider>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
+      </ClerkProvider>
     </Provider>
   );
 }
