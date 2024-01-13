07411826d18272ccd633f4fd8541b53f20ef402b
Romaric Zounlome <rzounlom@gmail.com>
2024-01-13 12:22:52 -0500

    chore: refactor state folders

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,6 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "../features/counter/couterSlice";
-// ...
+import counterReducer from "@/features/counter/couterSlice";
 
 export const store = configureStore({
   reducer: {
