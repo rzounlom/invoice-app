a9134985879232988e4dc1d6802d2e211961c9a7
Romaric Zounlome <rzounlom@gmail.com>
2024-01-18 04:44:09 -0500

    chore: remove unused types

--- src/types.ts
+++ src/types.ts
@@ -1,37 +1,5 @@
-import { INVOICE_STATUS } from "@prisma/client";
-
 export type DropdownItem = {
   id: number;
   name: "draft" | "pending" | "paid";
   label: string;
-};
-
-// invoice types
-
-export type Address = {
-  street: string;
-  city: string;
-  postCode: string;
-  country: string;
-};
-
-export type Item = {
-  name: string;
-  quantity: number;
-  price: number;
-  total: number;
-};
-
-export type Invoice = {
-  userId: string;
-  paymentDate: string;
-  description: String;
-  paymentTerms: number;
-  clientName: String;
-  clientEmail: String;
-  status: INVOICE_STATUS;
-  senderAddress: Address;
-  clientAddress: Address;
-  items: Item[];
-  total: number;
 };
