d5fe6e6ae518ad12d2f5fcf2df0758b2873be53d
Romaric Zounlome <rzounlom@gmail.com>
2024-01-18 06:37:44 -0500

    chore: add utility classes to format invoice data

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,8 @@
+export const formattedDate = (dateString: string): string => {
+  const date = new Date(dateString);
+  return new Intl.DateTimeFormat("en-US", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  }).format(date);
+};
---
+++ src/utils/formatNumber.ts
@@ -0,0 +1,7 @@
+export const formatNumber = (num: number): string => {
+  return new Intl.NumberFormat("en-US", {
+    style: "decimal",
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(num);
+};
