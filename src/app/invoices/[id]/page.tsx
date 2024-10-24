5c97a0b6cc1132dd7dbb8ecf32e41628146a0173
Romaric Zounlome <rzounlom@gmail.com>
2024-10-24 07:57:34 -0400

    add default single invoice page

--- src/app/invoices/[id]/page.tsx
+++ src/app/invoices/[id]/page.tsx
@@ -1,10 +1,14 @@
 import { FC } from "react";
 import InvoiceMain from "@/app/ui/invoices/invoices-main";
 
-const SingleInvoice: FC = () => {
+const SingleInvoice: FC = async () => {
   return (
-    <div className="w-full h-[calc(100vh-103px)] xl:w-[calc(100vw-103px)]">
-      <InvoiceMain>Single Invoice</InvoiceMain>
+    <div className="w-full h-[calc(100vh-80px)] xl:w-[calc(100vw-103px)] xl:h-full">
+      <InvoiceMain>
+        <div className="border h-full w-full flex flex-col">
+          <div className="h-[91px] "></div>
+        </div>
+      </InvoiceMain>
     </div>
   );
 };
