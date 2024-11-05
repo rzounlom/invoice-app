23bc9bb5e80972dbb3ddb57ca8359daef23c5623
Romaric Zounlome <rzounlom@gmail.com>
2024-11-05 05:17:46 -0500

    update format date util

--- src/lib/utils/format-date.tsx
+++ src/lib/utils/format-date.tsx
@@ -1,4 +1,4 @@
-export function formatDate(dateString: string): string {
+export function formatDate(dateString: Date): string {
   const date = new Date(dateString);
 
   // Get the day, month, and year
