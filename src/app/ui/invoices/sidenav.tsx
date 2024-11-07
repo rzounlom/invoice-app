55fdd578f2a68f70d9651bbf4c7964d77bda6a3d
Romaric Zounlome <rzounlom@gmail.com>
2024-11-07 09:24:55 -0500

    update sidenav link

--- src/app/ui/invoices/sidenav.tsx
+++ src/app/ui/invoices/sidenav.tsx
@@ -16,16 +16,17 @@ const SideNav: FC = () => {
 
   return (
     <div className="h-[80px] w-full flex justify-between items-center xl:w-[103px] xl:h-full xl:flex-col bg-pale-navy dark:dark:bg-midnight-navy">
-      <div className="h-[80px] w-[80px] xl:h-[103px] xl:w-[103px] relative flex justify-center items-center">
-        <Link href="/invoices">
-          <Image
-            className="xl:hidden"
-            src="/logo-outer.svg"
-            alt="logo"
-            height={80}
-            width={80}
-          />
-        </Link>
+      <Link
+        href="/invoices"
+        className="h-[80px] w-[80px] xl:h-[103px] xl:w-[103px] relative flex justify-center items-center"
+      >
+        <Image
+          className="xl:hidden"
+          src="/logo-outer.svg"
+          alt="logo"
+          height={80}
+          width={80}
+        />
 
         <Image
           className="xl:hidden absolute z-10"
@@ -35,15 +36,13 @@ const SideNav: FC = () => {
           width={25}
         />
 
-        <Link href="/invoices">
-          <Image
-            className="hidden xl:block"
-            src="/logo-outer.svg"
-            alt="logo"
-            height={103}
-            width={103}
-          />
-        </Link>
+        <Image
+          className="hidden xl:block"
+          src="/logo-outer.svg"
+          alt="logo"
+          height={103}
+          width={103}
+        />
 
         <Image
           className="hidden xl:block absolute z-10"
@@ -52,7 +51,7 @@ const SideNav: FC = () => {
           height={38}
           width={40}
         />
-      </div>
+      </Link>
       <div className="h-full w-[192px] flex xl:flex-col xl:h-[192px] xl:w-full">
         <div className="h-full w-[50%] flex justify-center items-center border-r-[.5px] xl:border-r-[0px] border-muted-navy xl:w-full xl:h-[50%] xl:border-b-[.5px]">
           <Image
