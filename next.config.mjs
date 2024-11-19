9169390c43b37e51f51a7b1c37e3762716559e54
Romaric Zounlome <rzounlom@gmail.com>
2024-11-18 22:49:22 -0500

    add tailwind ui domain for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "tailwindui.com",
+        port: "",
+        pathname: "/plus/img/logos/**",
+      },
+    ],
+    dangerouslyAllowSVG: true, // Allow SVG images
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+};
 
 export default nextConfig;
