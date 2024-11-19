ac5495a9125b9296ff1fb00c81bef5ed10eea029
Romaric Zounlome <rzounlom@gmail.com>
2024-11-18 22:49:51 -0500

    update middleware to redirect user to /invoices page when signed in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-]);
+import { NextResponse } from "next/server";
+
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 
 export default clerkMiddleware(async (auth, request) => {
+  // Check if the user is signed in and accessing the home route
+  if (auth().userId && request.nextUrl.pathname === "/") {
+    const invoicesUrl = new URL("/invoices", request.url);
+    return NextResponse.redirect(invoicesUrl);
+  }
+
+  // Protect non-public routes
   if (!isPublicRoute(request)) {
     auth().protect();
   }
+
+  return NextResponse.next();
 });
 
 export const config = {
