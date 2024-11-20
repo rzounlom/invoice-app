37ac86e8020c675ed741b7bedd2a40b2583a5f8e
Romaric Zounlome <rzounlom@gmail.com>
2024-11-19 19:21:35 -0500

    minor change

--- src/app/invoices/layout.tsx
+++ src/app/invoices/layout.tsx
@@ -1,7 +1,5 @@
 import SideNav from "../ui/invoices/sidenav";
 
-export const experimental_ppr = true;
-
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col xl:flex-row overflow-hidden">
